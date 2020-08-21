aab2e4508318102c570e468f2ab7bc804c3ec35b
Dmitry <f_o_r_w_a_r_d@inbox.ru>
2020-08-21 17:10:49 +0500

    hw3

--- app/src/main/java/com/dmitry/pisarevskiy/abovezero/MainActivity.java
+++ app/src/main/java/com/dmitry/pisarevskiy/abovezero/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,12 +14,15 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static com.dmitry.pisarevskiy.abovezero.R.menu.settings;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    Spinner spCity;
+
+    private static String activityState;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,15 +52,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spCity = findViewById(R.id.spCity);
+
+        final Spinner spCity = findViewById(R.id.spCity);
 
         spCity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String item = spCity.getSelectedItem().toString();
+                String item = parent.getSelectedItem().toString();
                 if (item.equals("Добавить…")) {
                     startActivity(new Intent(MainActivity.this ,CityActivity.class));
-                    spCity.setVerticalScrollbarPosition(0);
+                    parent.setVerticalScrollbarPosition(0);
                 } else {
                     TextView textView = (TextView) parent.getChildAt(0);
                     textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
@@ -67,8 +72,66 @@ public class MainActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> arg0) {
             }
         });
+
+        if (savedInstanceState==null) {
+            showMsgAboutState("Первый запуск onCreate");
+        } else {
+            showMsgAboutState("Повторный запуск onCreate");
+        }
     }
 
 
+    private void showMsgAboutState(String msg) {
+        Toast.makeText(this,msg, Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity",msg);
+    }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showMsgAboutState("Старт");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        showMsgAboutState("Стоп");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showMsgAboutState("Resume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        showMsgAboutState("Пауза");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        showMsgAboutState("Рестарт");
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        showMsgAboutState("Нажата кнопка назад");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        showMsgAboutState("Приложение закрыто");
+    }
 }
--- app/src/main/java/com/dmitry/pisarevskiy/abovezero/SettingsActivity.java
+++ app/src/main/java/com/dmitry/pisarevskiy/abovezero/SettingsActivity.java
@@ -1,13 +1,23 @@
 package com.dmitry.pisarevskiy.abovezero;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 
 
 public class SettingsActivity extends Activity {
+    private EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +26,94 @@ public class SettingsActivity extends Activity {
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SettingsActivity.this, MainActivity.class));
+                finish();
             }
         });
+
+        editText = findViewById(R.id.etExample);
+
+        final CheckBox showWindSpeed = findViewById(R.id.cbShowWindSpeed);
+        final CheckBox showPressure = findViewById(R.id.cbShowPressure);
+        final Spinner windUnit = findViewById(R.id.spWindUnit);
+        final Spinner pressureUnit = findViewById(R.id.spPressureUnit);
+        final RadioGroup rgMode = findViewById(R.id.rgMode);
+        final RadioButton rbNightMode = findViewById(R.id.radioButton2);
+        final SingleTon settings = SingleTon.getInstance();
+
+        showWindSpeed.setChecked(settings.isShowWindSpeed());
+        showPressure.setChecked(settings.isShowPressure());
+        windUnit.setSelection(settings.getWindSpeedUnit());
+        pressureUnit.setSelection(settings.getPressureUnit());
+        if (settings.isNightMode()) {
+            rbNightMode.setChecked(true);
+        }
+        windUnit.setSelection(settings.getWindSpeedUnit());
+        pressureUnit.setSelection(settings.getPressureUnit());
+
+        showWindSpeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settings.setShowWindSpeed(showWindSpeed.isChecked());
+            }
+        });
+
+        showPressure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settings.setShowPressure(showPressure.isChecked());
+            }
+        });
+
+        rgMode.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case 0:
+                        settings.setNightMode(false);
+                        break;
+                    case R.id.radioButton2:
+                        settings.setNightMode(true);
+                        break;
+                }
+            }
+        });
+
+        windUnit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                settings.setWindSpeedUnit(position);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        pressureUnit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                settings.setPressureUnit(position);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("etExample", editText.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        editText.setText(savedInstanceState.getString("etExample"));
     }
 }
