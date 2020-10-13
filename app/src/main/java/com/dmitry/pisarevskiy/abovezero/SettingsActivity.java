fed86b21b83717ef53c78cccb6f698c6411dc2e1
Dmitry <f_o_r_w_a_r_d@inbox.ru>
2020-10-13 21:39:56 +0500

    hw8 - advanced

--- app/src/main/java/com/dmitry/pisarevskiy/abovezero/SettingsActivity.java
+++ app/src/main/java/com/dmitry/pisarevskiy/abovezero/SettingsActivity.java
@@ -15,13 +15,14 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
 
 
 public class SettingsActivity extends Activity {
-    public static boolean ok=false;
+    public static boolean ok = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +30,25 @@ public class SettingsActivity extends Activity {
         setContentView(R.layout.activity_settings);
         Button btnOk = findViewById(R.id.btnOk);
         Button btnCancel = findViewById(R.id.btnCancel);
-        final CheckBox showWindSpeed = findViewById(R.id.cbShowWindSpeed);
-        final CheckBox showPressure = findViewById(R.id.cbShowPressure);
-        final Spinner windUnit = findViewById(R.id.spWindUnit);
-        final Spinner pressureUnit = findViewById(R.id.spPressureUnit);
-        final RadioGroup rgMode = findViewById(R.id.rgMode);
-        final RadioButton rbNightMode = findViewById(R.id.radioButton2);
-        final SingleTon settings = SingleTon.getInstance();
-
+        // привязка View
+        final CheckBox cbShowWindSpeed = findViewById(R.id.cbShowWindSpeed);
+        final CheckBox cbShowPressure = findViewById(R.id.cbShowPressure);
+        final Spinner spWindUnit = findViewById(R.id.spWindUnit);
+        final Spinner spPressureUnit = findViewById(R.id.spPressureUnit);
+        // инициализация значений
+        cbShowWindSpeed.setChecked(MainActivity.showWind);
+        cbShowPressure.setChecked(MainActivity.showPressure);
+        if (MainActivity.windUnit.equals("м/с")) {
+            spWindUnit.setSelection(0);
+        } else {
+            spWindUnit.setSelection(1);
+        }
+        if (MainActivity.pressureUnit.equals("кПа")) {
+            spPressureUnit.setSelection(0);
+        } else {
+            spPressureUnit.setSelection(1);
+        }
+        // установка слушателей
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,10 +60,10 @@ public class SettingsActivity extends Activity {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 Intent intent = new Intent();
-                                intent.putExtra(MainActivity.PRESSURE_SHOW_TAG, showPressure.isChecked());
-                                intent.putExtra(MainActivity.WIND_SHOW_TAG, showWindSpeed.isChecked());
-                                intent.putExtra(MainActivity.WIND_UNIT_TAG, String.valueOf(windUnit.getSelectedItem()));
-                                intent.putExtra(MainActivity.PRESSURE_UNIT_TAG, String.valueOf(pressureUnit.getSelectedItem()));
+                                intent.putExtra(MainActivity.PRESSURE_SHOW_TAG, cbShowPressure.isChecked());
+                                intent.putExtra(MainActivity.WIND_SHOW_TAG, cbShowWindSpeed.isChecked());
+                                intent.putExtra(MainActivity.WIND_UNIT_TAG, String.valueOf(spWindUnit.getSelectedItem()));
+                                intent.putExtra(MainActivity.PRESSURE_UNIT_TAG, String.valueOf(spPressureUnit.getSelectedItem()));
                                 setResult(MainActivity.RESULT_OK, intent);
                                 finish();
                             }
@@ -70,10 +82,8 @@ public class SettingsActivity extends Activity {
                         });
                 AlertDialog alert = builder.create();
                 alert.show();
-
             }
         });
-
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,59 +92,27 @@ public class SettingsActivity extends Activity {
                 finish();
             }
         });
-
-        showWindSpeed.setChecked(settings.isShowWindSpeed());
-        showPressure.setChecked(settings.isShowPressure());
-        windUnit.setSelection(settings.getWindSpeedUnit());
-        pressureUnit.setSelection(settings.getPressureUnit());
-        if (settings.isNightMode()) {
-            rbNightMode.setChecked(true);
-        }
-        windUnit.setSelection(settings.getWindSpeedUnit());
-        pressureUnit.setSelection(settings.getPressureUnit());
-
-        showWindSpeed.setOnClickListener(new View.OnClickListener() {
+        cbShowWindSpeed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                settings.setShowWindSpeed(showWindSpeed.isChecked());
             }
         });
-
-        showPressure.setOnClickListener(new View.OnClickListener() {
+        cbShowPressure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                settings.setShowPressure(showPressure.isChecked());
             }
         });
-
-        rgMode.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId) {
-                    case 0:
-                        settings.setNightMode(false);
-                        break;
-                    case R.id.radioButton2:
-                        settings.setNightMode(true);
-                        break;
-                }
-            }
-        });
-
-        windUnit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spWindUnit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                settings.setWindSpeedUnit(position);
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
-
-        pressureUnit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spPressureUnit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                settings.setPressureUnit(position);
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
